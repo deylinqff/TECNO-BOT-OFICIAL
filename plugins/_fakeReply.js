271b43fc36d18a795ceb49ad98321b2641ef4bf9
Niño Piña <ninopina10@gmail.com>
2024-11-11 14:05:23 -0600

    Actualizar plugins

---
+++ plugins/_fakeReply.js
@@ -0,0 +1,49 @@
+import fetch from 'node-fetch'
+
+export async function before(m, { conn }) {
+let name = '𝑪𝒓𝒐𝒘𝑩𝒐𝒕- 𝑴𝑫 - 𝑪𝒉𝒂𝒏𝒏𝒆𝒍'
+let imagenes = ["https://iili.io/dtLKOBI.md.jpg",
+"https://iili.io/dtLBx1I.md.jpg",
+"https://iili.io/dtLCWFI.md.jpg",
+"https://iili.io/dtLnzRn.md.jpg",
+"https://iili.io/dtLoqzb.md.jpg",
+"https://iili.io/dtLx2GR.md.jpg",
+"https://iili.io/dtLxM6Q.md.jpg"]
+
+let icono = imagenes[Math.floor(Math.random() * imagenes.length)]
+
+
+global.rcanal = {
+ contextInfo: {
+             isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363287436060300@newsletter",
+      serverMessageId: 100,
+      newsletterName: name,
+   }, 
+   externalAdReply: {
+    showAdAttribution: true, 
+    title: botname, 
+    body: textbot, 
+    mediaUrl: null, 
+    description: null, 
+    previewType: "PHOTO", 
+    thumbnailUrl: icono, 
+    sourceUrl: canal, 
+    mediaType: 1, 
+    renderLargerThumbnail: false }, 
+    }, 
+    }
+
+
+ global.fake = {
+    contextInfo: {
+            isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: "120363287436060300@newsletter",
+      serverMessageId: 100,
+      newsletterName: name,
+    },
+    },
+  }
+}

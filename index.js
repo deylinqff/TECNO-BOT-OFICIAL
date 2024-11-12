6a4ddb1058f4b6511208be7ca18d015a9de96ad0
Niño Piña <ninopina10@gmail.com>
2024-11-12 16:22:03 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let require = createRequire(__dirname)
 let { say } = cfonts
 let rl = createInterface(process.stdin, process.stdout)
 
-say('Naruto', {
+say('Crow', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']

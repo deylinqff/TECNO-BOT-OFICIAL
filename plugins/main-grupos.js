41a8dd872d62e35561bc7abd8599f967c325f0a7
Niño Piña <ninopina10@gmail.com>
2024-11-22 16:30:10 -0600

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
-let img = await (await fetch(`https://i.ibb.co/1bhcmQg/Sylph.jpg`)).buffer()
+let img = await (await fetch(`https://files.catbox.moe/ge77oy.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D*
+let txt = `*Hola!, te invito a unirte a los grupos oficiales de de CrowBot-Ai para convivir con la comunidad :D*
 
 *✰* ${group}
 

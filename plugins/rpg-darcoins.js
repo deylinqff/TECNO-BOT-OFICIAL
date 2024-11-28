b71cd17646b83c74553b6221251e8ceaad31e304
Niño Piña <ninopina10@gmail.com>
2024-11-28 12:57:33 -0600

    Actualizar rpg-darcoins.js

--- plugins/rpg-darcoins.js
+++ plugins/rpg-darcoins.js
@@ -6,15 +6,15 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw '🚩 Menciona al usuario con *@user.*'
+    if (!who) throw '🌠 MENCIONA AL USUARIO *@user.*'
     let txt = text.replace('@' + who.split`@`[0], '').trim()
-    if (!txt) throw '🚩 Ingrese la cantidad de *❇️ Eris* que quiere transferir.'
-    if (isNaN(txt)) throw 'Sólo números.'
+    if (!txt) throw '🌠 INGRESE LA CANTIDAD DE *🌠 ESTRELLAS* QUE DESEA TRANSFERIR.'
+    if (isNaN(txt)) throw 'SOLO NÚMEROS 🔢.'
     let poin = parseInt(txt)
     let limit = poin
     let imt = Math.ceil(poin * impuesto)
     limit += imt
-    if (limit < 1) throw '🚩 Mínimo es *1 ❇️ Eris*.'
+    if (limit < 1) throw '🏦 MÍNIMO *1 🌠 ESTRELLAS*.'
     let users = global.db.data.users
     if (limit > users[m.sender].limit) throw 'No tienes suficientes *❇️ Eris* para dar.'
     users[m.sender].limit -= limit

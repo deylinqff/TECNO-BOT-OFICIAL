3531caf90750a74e061ea0b596755838d95e4bdb
Niño Piña <ninopina10@gmail.com>
2024-11-28 11:57:19 -0600

    Actualizar downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,8 +1,8 @@
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args || !args[0]) return conn.reply(m.chat, '🚩 Ingresa un enlace del vídeo de TikTok junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://vm.tiktok.com/ZMrFCX5jf/`, m, rcanal)
-    if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `Verifica que el link sea de TikTok`, m, rcanal).then(_ => m.react('✖️'))
+if (!args || !args[0]) return conn.reply(m.chat, '🧑‍💻 INGRESE EL LINK DEL VIDEO DE TIKTOK.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://vm.tiktok.com/ZMrFCX5jf/`, m, rcanal)
+    if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `🧑‍💻VERIFICA QUE EL LINK SEA DE TIKTOK`, m, rcanal).then(_ => m.react('✖️'))
   await m.react('🕓')
 try {
 let { title, author, duration, views, likes, comment, share, published, downloads, dl_url } = await Starlights.tiktokdl(args[0])
@@ -16,7 +16,7 @@ let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n'
     txt += `	✩  *Compartidos* : ${share}\n`
     txt += `	✩  *Publicado* : ${published}\n`
     txt += `	✩  *Descargas* : ${downloads}\n\n`
-    txt += `> 🚩 *${textbot}*`
+    txt += `> 🧑‍💻 *${textbot}*`
 await conn.sendFile(m.chat, dl_url, 'tiktok.mp4', txt, m, null, rcanal)
 await m.react('✅')
 } catch {

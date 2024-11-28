7c237f5dc6e214285a8ad594e606293c8cce0557
Niño Piña <ninopina10@gmail.com>
2024-11-28 12:13:10 -0600

    Actualizar audio-efectos.js

--- plugins/audio-efectos.js
+++ plugins/audio-efectos.js
@@ -45,7 +45,7 @@ let handler = async (m, { conn, args, __dirname, usedPrefix, command }) => {
                 })
                 await m.react('✅')
             })
-        } else return conn.reply(m.chat, `🚩 Responde al *Audio o Nota de Voz.*`, m, rcanal)
+        } else return conn.reply(m.chat, `🧑‍💻 RESPONDA AL AUDIO O NOTA DE VOZ 🎵*`, m, rcanal)
     } catch {
         await m.react('✖️')
     }

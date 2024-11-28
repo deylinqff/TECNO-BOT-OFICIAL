79130a9fdfcd8a2d065b336504bd99e467437165
Niño Piña <ninopina10@gmail.com>
2024-11-28 12:21:37 -0600

    Actualizar game-casino.js

--- plugins/game-casino.js
+++ plugins/game-casino.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn, args, usedPrefix, command, DevMode }) => {
 	    
 	    if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
     let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    conn.reply(m.chat, `🚩 Ya has iniciado una apuesta recientemente, espera *⏱ ${tiempoRestante}* para apostar nuevamente`, m, rcanal)
+    conn.reply(m.chat, `🎰 YA HAS INICIADO UNA APUESTA, ESPERA *⏱ ${tiempoRestante}* PARA VOLVER APOSTAR`, m, rcanal)
     return
   }
   
@@ -25,20 +25,20 @@ let handler = async (m, { conn, args, usedPrefix, command, DevMode }) => {
 	    
         count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
         count = Math.max(1, count)
-        if (args.length < 1) return conn.reply(m.chat, '🚩 Ingresa la cantidad de ' + `*❇️ Estrellas*` + ' que deseas aportar contra' + ` *Crow Ai*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+        if (args.length < 1) return conn.reply(m.chat, '🚩 INGRESE LA CANTIDAD DE' + `*🌠 Estrellas*` + ' QUE DESEA APOSTAR CONTRA' + ` *CrowBot-Ai*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
 
         if (user.limit >= count * 1) {
             user.limit -= count * 1
             if (Aku > Kamu) {
-                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *Crow Ai* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} ❇️ Estrellas.`.trim(), m, rcanal)
+                conn.reply(m.chat, '`🎲 VEAMOS QUE NUMEROS TIENEN!`\n\n'+ `➠ *CrowBot-Ai* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🌠 Estrellas.`.trim(), m, rcanal)
             } else if (Aku < Kamu) {
                 user.limit += count * 2
-                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *Crow Ai* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} ❇️ Estrellas.`.trim(), m, rcanal)
+                conn.reply(m.chat, '`🎲 VEAMOS QUE NÚMEROS TIENEN!`\n\n'+ `➠ *Crow Ai* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🌠 Estrellas.`.trim(), m, rcanal)
             } else {
                 user.limit += count * 1
-                conn.reply(m.chat, '`🌵 Veamos que numeros tienen!`\n\n'+ `➠ *CrowBot Ai* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} ❇️ Estrellas.`.trim(), m, rcanal)
+                conn.reply(m.chat, '`🎲VEAMOS QUE NÚMEROS TIENEN!`\n\n'+ `➠ *CrowBot Ai* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🌠 Estrellas.`.trim(), m, rcanal)
             }
-        } else conn.reply(m.chat, `No tienes *${formatNumber(count)} ❇️ Estrellas* para apostar!`.trim(), m, rcanal)
+        } else conn.reply(m.chat, `🤖NO TIENES *${formatNumber(count)} 🌠 Estrellas* PARA APOSTAR!`.trim(), m, rcanal)
     
 }
     

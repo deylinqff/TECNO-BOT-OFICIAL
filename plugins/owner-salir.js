aa6a9e5555f2e72a133a6a3d8802e45b8bd1cc43
Niño Piña <ninopina10@gmail.com>
2024-11-28 12:36:18 -0600

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
 let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
-await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! (≧ω≦)ゞ*', mentions: [m.sender] }, { quoted: estilo })
+await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*ADIOS, CROWBOT SE DESPIDE!💫👋*', mentions: [m.sender] }, { quoted: estilo })
 await conn.groupLeave(id)}
 handler.help = ['salir']
 handler.tags = ['owner']

65fda3c565356bf99f1f8527958ddae47d829f6d
Niño Piña <ninopina10@gmail.com>
2024-11-28 13:15:38 -0600

    Actualizar downloader-aptoide.js

--- plugins/downloader-aptoide.js
+++ plugins/downloader-aptoide.js
@@ -1,12 +1,12 @@
 import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `*• Ingresa el nombre de la aplicación que deseas descargar.*\n\nEjemplo:\n*${usedPrefix + command}* WhatsApp`, m, rcanal)
+if (!text) return conn.reply(m.chat, `*🤖Ingresa el nombre de la aplicación que deseas descargar.*\n\nEjemplo:\n*${usedPrefix + command}* WhatsApp`, m, rcanal)
 await m.react('🕓')
 try {
 let { name, packname, update, size, thumbnail, dl_url } = await Starlights.aptoide(text)
 if (size.includes('GB') || size.replace(' MB', '') > 300) { return await m.reply('El archivo pesa mas de 300 MB, se canceló la Descarga.')}
-let txt = `*乂  A P T O I D E  -  D O W N L O A D*\n\n`
+let txt = `*乂  C R O W  -  A P T O I D E*\n\n`
     txt += `	✩   *Nombre* : ${name}\n`
     txt += `	✩   *Package* : ${packname}\n`
     txt += `	✩   *Update* : ${update}\n`

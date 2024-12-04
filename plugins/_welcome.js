dcf177ec1bde9d80bb7efc444315f18612c96011
Niño Piña <ninopina10@gmail.com>
2024-12-03 19:52:54 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -3,20 +3,22 @@ import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let img = imagen1
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/V3Hsgcy/file.jpg')
+  let img = 'https://i.ibb.co/V3Hsgcy/file.jpg')).buffer()
   let chat = global.db.data.chats[m.chat]
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let welcome = ` ⛄ *≺ CROW BOT - AI* \n「 Bιҽɳʋҽɳιԃσ 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Bιҽɳʋҽɳιԃσ/α 」\n「 ${groupMetadata.subject} 」\n\n> *WillZek*`
-await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
+  if (chat.bienvenida && m.messageStubType == 27) {
+    let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+
+await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
   }
 
-  if (chat.welcome && m.messageStubType == 28) {
-    let bye = ` ⛄ *≺ CROW BOT- AI* \n「 Aԃισʂ 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Sҽ ϝυҽ 」\n「 Nυɳƈα ƚҽ ϙυιʂιɱσʂ αϙυι 」\n\n> *WillZek*`
-await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, redes, fkontak)
+  if (chat.bienvenida && m.messageStubType == 28) {
+    let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se Salió Un Puto\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
   }
 
-  if (chat.welcome && m.messageStubType == 32) {
-    let kick = ` ⛄ *≺ CROW BOT - AI* \n「 Aԃισʂ 」\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n「 Sҽ ϝυҽ 」\n「 Nυɳƈα ƚҽ ϙυιʂιɱσʂ αϙυι 」\n\n> *WillZek*`
-await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, redes, fkontak)
+  if (chat.bienvenida && m.messageStubType == 32) {
+    let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se Salio Un Puto\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
 }}

3325c76107298455f3067d5ca66bda1fbf7cb483
Niño Piña <ninopina10@gmail.com>
2024-12-04 10:13:25 -0600

    Actualizar ComprarBot.js

--- plugins/ComprarBot.js
+++ plugins/ComprarBot.js
@@ -1,15 +1,22 @@
-const handler = async (m, {conn}) => {
-  m.reply(global.ComprarBot);
-};
-handler.command = /^(ComprarBot|Comprar|comprar|ComprarBot)$/i;
+// Créditos A Niño Piña
+let handler = async (m, { conn }) => {
+// No Quites Los Créditos🦌
+m.react('🎄');
+// Mensaje que se enviará
+const message = "〔⛄ *CROW - BOT* ❄️〕\n\n> *🦌FELIZ NAVIDAD🎄*\n\n*BOT PARA GRUPO* :\n> wa.me/50557865603\n\n*BOT PERZONALIZADO* :\n> wa.me/50557865603";
+if (m, rcanal) {
+// Feliz Navidad🎄
+const imageUrl = 'https://i.ibb.co/qJNL5Bg/file.jpg';
+try {
+// Que No Quites Los Créditos😑
+// Te Estoy Viendo😑
+await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message, mimetype: 'image/jpeg' });
+} catch (error) {
+console.error('Error al enviar el mensaje:', error);
+}
+}
+}
+handler.help = ['comprar'];
+handler.tags = ['main'];
+handler.command = ['comprar'];
 export default handler;
-
-global.ComprarBot = `
-〔⛄ *CROW - BOT* ❄️〕\n\n> *🦌FELIZ NAVIDAD🎄*
-
-*BOT PARA GRUPO* :
-> wa.me/50557865603
-
-*BOT PERZONALIZADO* :
-> wa.me/50557865603
-`;

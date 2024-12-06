3634e48502dd6bf9a217c85faccbbe4bc636b896
Niño Piña <ninopina10@gmail.com>
2024-12-06 10:06:58 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -11,7 +11,8 @@ import axios from 'axios'
 global.owner = [
   ['50557865603', 'WillZek', true],
   ['50557865603', 'CrowBot-Ai', true],
-  ['584241836217', 'Prak Harper', true]
+  ['584241836217', 'Prak Harper', true],
+  ['50498409019', 'WillZek-Soporte', true]
 ]
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*

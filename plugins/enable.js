fc75ea1bb28739fc86a5cca7c536331f39cbc304
Niño Piña <ninopina10@gmail.com>
2024-12-08 14:19:42 -0600

    Actualizar enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -37,7 +37,20 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
     isUser = true
     user.useDocument = isEnable
     break
- 
+
+      case 'autoaceptar': case 'aceptarnuevos':
+if (!m.isGroup) {
+if (!isOwner) {
+global.dfail('group', m, conn)
+throw false
+}
+} else if (!isAdmin) {
+global.dfail('admin', m, conn)
+throw false
+}
+chat.autoAceptar = isEnable
+break
+
     case 'antilink':
       if (m.isGroup) {
         if (!(isAdmin || isOwner)) {

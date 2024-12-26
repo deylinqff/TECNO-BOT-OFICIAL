b65a70184826f95acc81e2b0db1f5b30d46e9e99
Deylinel <deylibaquedano40@gmail.com>
2024-12-26 15:18:59 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,24 +1,52 @@
-import {WAMessageStubType} from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
+import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
 
-export async function before(m, {conn, participants, groupMetadata}) {
+export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/V3Hsgcy/file.jpg')
-  let img = await (await fetch(`${pp}`)).buffer()
-  let chat = global.db.data.chats[m.chat]
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/V3Hsgcy/file.jpg');
+  let img = await (await fetch(`${pp}`)).buffer();
+  let chat = global.db.data.chats[m.chat];
 
   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+    let bienvenida = `╔═══❖•ೋ° °ೋ•❖═══╗
+╔═════ ▓▓ ࿇ ▓▓ ═════╗
+╔╦══• •✠•❀•✠ • •══╦╗
+✦ 𝔹𝕀𝔼ℕ𝕍𝔼ℕ𝕀𝔻𝕆 ✦
+╚╩══• •✠•❀•✠ • •══╩╝
+✧ Usuario: @${m.messageStubParameters[0].split`@`[0]}  
+✧ Grupo: ${groupMetadata.subject}  
+✧ Sistema: ¡Conexión establecida!  
+╚═════ ▓▓ ࿇ ▓▓ ═════╝
+╚═══❖•ೋ° °ೋ•❖═══╝`;
 
-await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
+    await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo);
   }
 
   if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
-await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
+    let bye = `╔═══❖•ೋ° °ೋ•❖═══╗
+╔═════ ▓▓ ࿇ ▓▓ ═════╗
+╔╦══• •✠•❀•✠ • •══╦╗
+✦ 𝔻𝔼𝕊ℙ𝔼𝔻𝕀𝔻𝔸 ✦
+╚╩══• •✠•❀•✠ • •══╩╝
+✧ Usuario: @${m.messageStubParameters[0].split`@`[0]}  
+✧ Mensaje: Usuario desconectado del sistema.  
+╚═════ ▓▓ ࿇ ▓▓ ═════╝
+╚═══❖•ೋ° °ೋ•❖═══╝`;
+
+    await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo);
   }
 
   if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
-await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
-}}
+    let kick = `╔═══❖•ೋ° °ೋ•❖═══╗
+╔═════ ▓▓ ࿇ ▓▓ ═════╗
+╔╦══• •✠•❀•✠ • •══╦╗
+✦ 𝔼𝕏ℙ𝕌𝕃𝕊𝔸𝔻𝕆 ✦
+╚╩══• •✠•❀•✠ • •══╩╝
+✧ Usuario: @${m.messageStubParameters[0].split`@`[0]}  
+✧ Mensaje: Usuario eliminado de la base de datos.  
+╚═════ ▓▓ ࿇ ▓▓ ═════╝
+╚═══❖•ೋ° °ೋ•❖═══╝`;
+
+    await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo);
+  }
+}

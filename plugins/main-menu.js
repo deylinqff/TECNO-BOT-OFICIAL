7736cf3fb08f60f43fa3657dd77a5629e6ca88cd
Deylinel <deylibaquedano40@gmail.com>
2024-12-26 14:25:44 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -201,7 +201,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let img = ['https://files.catbox.moe/rxd1l7.jpg',
 'https://files.catbox.moe/w0zxvw.jpg',
 'https://files.catbox.moe/z7ru19.jpg',
-'https://files.catbox.moe/5ux2ru.jpg',]
+'https://files.catbox.moe/5ux2ru.jpg']
     await m.react('🚀')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)

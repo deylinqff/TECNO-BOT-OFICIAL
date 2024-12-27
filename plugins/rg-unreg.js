8984f9629d1c6e97fa9a0cce077ce89f6e96cd4b
Deylinel <deylibaquedano40@gmail.com>
2024-12-26 20:21:09 -0600

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -2,7 +2,7 @@ let handler = async function (m, { conn, args, usedPrefix }) {
   let user = global.db.data.users[m.sender]
   if (!user.registered) return m.reply(`🚩 No tienes un registro activo.`)
   user.registered = false
-  m.reply(`🚩 Registro eliminado correctamente.`)
+  m.reply(`🚀 Registro eliminado correctamente⚙️.`)
 }
 handler.help = ['unreg'] 
 handler.tags = ['rg']

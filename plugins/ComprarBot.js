32669b6b3b878de005cd464a32e016e1e69abd60
Deylinel <deylibaquedano40@gmail.com>
2024-12-28 11:53:28 -0600

    Actualizar ComprarBot.js

--- plugins/ComprarBot.js
+++ plugins/ComprarBot.js
@@ -1,11 +1,11 @@
-// Créditos A Niño Piña
+// Créditos A Deylin
 let handler = async (m, { conn }) => {
-// No Quites Los Créditos🦌
-m.react('🎄');
+// No Quites Los Créditos🚀
+m.react('⚙️');
 // Mensaje que se enviará
-const message = "〔⛄ *CROW - BOT* ❄️〕\n\n> *🦌FELIZ NAVIDAD🎄*\n\n*BOT PARA GRUPO* :\n> wa.me/50557865603\n\n*BOT PERZONALIZADO* :\n> wa.me/50557865603";
+const message = "〔🚀 *TECNO-BOT* 🚀〕\n\n> \n\n*BOT PARA GRUPO* :\n> wa.me/50488198573\n\n*BOT PERZONALIZADO* :\n> wa.me/50488198573";
 if (m, rcanal) {
-// Feliz Navidad🎄
+//
 const imageUrl = 'https://i.ibb.co/qJNL5Bg/file.jpg';
 try {
 // Que No Quites Los Créditos😑

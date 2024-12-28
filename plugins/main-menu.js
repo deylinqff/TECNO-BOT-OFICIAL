9b1574229139b600a9ceaf7f00f873e5af134a8c
Deylinel <deylibaquedano40@gmail.com>
2024-12-28 15:21:46 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -204,7 +204,6 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   'https://i.ibb.co/9WrytGt/file.jpg',
   'https://i.ibb.co/JmcS3kv/Sylph.jpg',
   'https://i.ibb.co/Cs6Tt9V/Sylph.jpg'
-];
     await m.react('🚀')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)

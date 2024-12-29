2adde5e4beb4128e622de0492fd903b757e00f70
Deylinel <deylibaquedano40@gmail.com>
2024-12-29 11:20:44 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,18 +1,18 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {  
     let txt_owner = `  
-╭━━━━━━━━━━━━━━━━━━━━━⬣  
-┃ *💌 Hola!*  
-┃  
-┃ 𝙴𝚂𝚃𝙴 𝙴𝚂 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝙼𝙸 
-┃ 𝙲𝚁𝙴𝙰𝙳𝙾𝚁.       
-┃ 𝙲𝚄𝙰𝙻𝚀𝚄𝙸𝙴𝚁 
-┃ 𝙵𝙰𝙻𝙻𝙰 𝙾 𝚂𝙸 𝚀𝚄𝙸𝙴𝚁𝙴𝚂 𝙰𝙶𝚁𝙴𝙶𝙰𝚁  
-┃ 𝙴𝙻 𝙱𝙾𝚃 𝙰 𝚃𝚄 𝙶𝚁𝚄𝙿𝙾, 𝙿𝚄𝙴𝙳𝙴𝚂  
-┃ 𝙷𝙰𝙱𝙻𝙰𝚁𝙻𝙴! 
-┃  
-┃ 📞 *Deylin*:  
-┃ 🌐 [Wa.me/50488198573] 
-╰━━━━━━━━━━━━━━━━━━━━━⬣ 
+⚡ *𝗖𝗢𝗡𝗘𝗫𝗜Ó𝗡 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟* ⚡  
+
+✨ *¡Bienvenido a la Red!* ✨  
+
+🚀 *¿Problemas con el Bot?*  
+Aquí tienes el contacto de mi *𝗖𝗥𝗘𝗔𝗗𝗢𝗥*.  
+
+💡 *¿Quieres agregar el Bot a tu grupo?*  
+Escríbele y lo solucionará de inmediato.  
+
+🌌 *𝗖𝗥𝗘𝗔𝗗𝗢𝗥*:  
+📞 *Deylin*:  
+🌐 [𝗪𝗮.𝗺𝗲/𝟱𝟬𝟰𝟴𝟴𝟭𝟵𝟴𝟱𝟳𝟯]  
 `;  
 
     await conn.sendFile(  
@@ -28,4 +28,4 @@ handler.help = ['owner'];
 handler.tags = ['main'];  
 handler.command = ['owner', 'creator', 'creador', 'dueño'];  
 
-export default handler; 
+export default handler;

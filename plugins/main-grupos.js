a953b561a1e5f191daa0ae494d057128af2f01d4
Deylinel <deylibaquedano40@gmail.com>
2024-12-28 22:48:54 -0600

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -4,7 +4,7 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 let img = await (await fetch(`https://files.catbox.moe/qzmn89.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-let txt = `*Hola!, te invito a unirte a los grupos oficiales de de TECNO-BOT para convivir con la comunidad :D*
+let txt = `*Hola!, te invito a unirte a los grupos oficiales de TECNO-BOT para convivir con la comunidad :D*
 
 *✰* ${group}
 

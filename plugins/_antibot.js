8afc377d557c264e168b44e765de1dd3ac4298de
Deylinel <deylibaquedano40@gmail.com>
2025-01-01 17:26:03 -0600

    Actualizar _antibot.js

--- plugins/_antibot.js
+++ plugins/_antibot.js
@@ -1,20 +1,51 @@
+// Comando para habilitar antiBot
+export async function enableAntiBot(m, { conn, isAdmin }) {
+    if (!isAdmin) {
+        return conn.reply(m.chat, "¡Solo los administradores pueden activar esta opción! ⚠️", m);
+    }
+    
+    let chat = global.db.data.chats[m.chat];
+
+    // Habilitar la opción antiBot
+    chat.antiBot = true;
+
+    await conn.reply(m.chat, "¡Protección anti-bot activada! 🚫🤖", m);
+}
+
+// Comando para deshabilitar antiBot
+export async function disableAntiBot(m, { conn, isAdmin }) {
+    if (!isAdmin) {
+        return conn.reply(m.chat, "¡Solo los administradores pueden desactivar esta opción! ⚠️", m);
+    }
+    
+    let chat = global.db.data.chats[m.chat];
+
+    // Deshabilitar la opción antiBot
+    chat.antiBot = false;
+
+    await conn.reply(m.chat, "¡Protección anti-bot desactivada! ✅", m);
+}
+
+// Función que antes revisa si se activa el antiBot
 export async function before(m, { conn, isAdmin, isBotAdmin }) {
     if (!m.isGroup) return;
     let chat = global.db.data.chats[m.chat]
     let delet = m.key.participant
     let bang = m.key.id
     let bot = global.db.data.settings[this.user.jid] || {}
+
     if (m.fromMe) return true;
 
     if (m.id.startsWith('3EB0') && m.id.length === 22) {
         let chat = global.db.data.chats[m.chat];
 
         if (chat.antiBot) {
-       //     await conn.reply(m.chat, "     ͞ ͟͞ ͟𝐓𝐞𝐜𝐧𝐨-𝐁𝐨𝐭 ͟͞ ͞   \n╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝\n\n𝑺𝒐𝒚 𝗧𝗲𝗰𝗻𝗼-𝘽𝙤𝙩 𝒆𝒍 𝒎𝒆𝒋𝒐𝒓 𝒃𝒐𝒕 𝒅𝒆 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑!!\n𝑬𝒔𝒕𝒆 𝒈𝒓𝒖𝒑𝒐 𝒏𝒐 𝒕𝒆 𝒏𝒆𝒔𝒆𝒄𝒊𝒕𝒂, 𝒂𝒅𝒊𝒐𝒔𝒊𝒕𝒐 𝒃𝒐𝒕 𝒅𝒆 𝒔𝒆𝒈𝒖𝒏𝒅𝒂.", null, rcanal);
+            // Mensaje que se enviaría si el bot detecta un mensaje sospechoso de un bot
+            await conn.reply(m.chat, "¡Un bot ha sido detectado en el grupo! El bot será eliminado si es necesario.", m);
 
             if (isBotAdmin) {
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+                await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+                await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
             }
         }
     }

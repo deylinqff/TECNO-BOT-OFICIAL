74d6f5c59a5ca007a348fb27f7341e95f2f7c6a6
Deylinel <deylibaquedano40@gmail.com>
2025-01-01 15:09:56 -0600

    Actualizar download-ig.js

--- plugins/download-ig.js
+++ plugins/download-ig.js
@@ -20,9 +20,9 @@ await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🎞️ *Tu video de in
 await m.react(error)
 conn.reply(m.chat, '⚙️ Ocurrió un error.', m, fake)}}
 
-handler.command = ['instagram2', 'ig2']
+handler.command = ['instagram', 'ig']
 handler.tags = ['descargas']
-handler.help = ['instagram2', 'ig2']
+handler.help = ['instagram', 'ig']
 //handler.yenes = 1
 handler.group = true;
 handler.register = true

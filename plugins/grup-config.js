5d71c91c24b7ba6f6ea6fc71582feda1025de3de
Deylinel <deylibaquedano40@gmail.com>
2025-01-01 00:24:13 -0600

    Actualizar grup-config.js

--- plugins/grup-config.js
+++ plugins/grup-config.js
@@ -1,13 +1,13 @@
 
 var handler = async (m, {conn, args, usedPrefix, command}) => {
-const isClose = { 'open': 'not_announcement', 'close': 'announcement', 'abierto': 'not_announcement', 'cerrado': 'announcement', 'abrir': 'not_announcement', 'cerrar': 'announcement', 'desbloquear': 'unlocked', 'bloquear': 'locked' }[(args[0] || '')]
-if (isClose === undefined) { return conn.reply(m.chat, `*Elija una opción para configurar el grupo*\n\nEjemplo:\n*○ !${command} abrir*\n*○ !${command} cerrar*\n*○ !${command} bloquear*\n*○ !${command} desbloquear*`, m, rcanal, )}
+const isClose = { 'open': 'not_announcement', 'close': 'announcement', 'abierto': 'not_announcement', 'cerrado': 'announcement', 'abierto': 'not_announcement', 'cerrado': 'announcement', 'desbloquear': 'unlocked', 'bloquear': 'locked' }[(args[0] || '')]
+if (isClose === undefined) { return conn.reply(m.chat, `*Elija una opción para configurar el grupo*\n\nEjemplo:\n*○ !${command} abierto*\n*○ !${command} cerrado*\n*○ !${command} bloquear*\n*○ !${command} desbloquear*`, m, rcanal, )}
 await conn.groupSettingUpdate(m.chat, isClose)
 { 
 conn.reply(m.chat, '✅ *Configurado correctamente*', m, rcanal, )
 await m.react(done)
 }}
-handler.help = ['group abrir / cerrar']
+handler.help = ['group abierto / cerrado']
 handler.tags = ['grupo']
 handler.command = /^(group|grupo)$/i
 handler.admin = true

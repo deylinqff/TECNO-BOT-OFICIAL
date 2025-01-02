5c95bc9df25ac6909efb118fa061864dece445af
Deylinel <deylibaquedano40@gmail.com>
2025-01-01 20:14:15 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,46 +1,24 @@
- import {WAMessageStubType} from '@whiskeysockets/baileys'
+import {WAMessageStubType} from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/t9l5f4.jpg')
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://i.ibb.co/V3Hsgcy/file.jpg')
   let img = await (await fetch(`${pp}`)).buffer()
   let chat = global.db.data.chats[m.chat]
 
   if (chat.bienvenida && m.messageStubType == 27) {
-    if (chat.sWelcome) {
-      let user = `@${m.messageStubParameters[0].split`@`[0]}`
-      let welcome = chat.sWelcome.replace('@user', () => user);
-      await conn.sendAi(m.chat, botname, textbot, welcome, img, img, canal)
-    } else {
-      let bienvenida = `╔══⏤͟͟͞͞𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕 \n║「 Bienvenido 」\n╚╦★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   ║✑  Bienvenido a\n   ║✑  ${groupMetadata.subject}\n   *╚╦══❖⏤͟͟͞͞𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕❖═══╝*
-╔═╝
-*╠═══❖•ೋ° °ೋ•❖═══╗*
-https://chat.whatsapp.com/DQLT8n8IghUHYdkD2YLLnz
-*╚═══❖•ೋ° °ೋ•❖═══╝*
-`
-      await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal)
-    }
+    let bienvenida = `┌─★ *${botname}* \n│「 Bienvenido 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Bienvenido a\n   │✑  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+
+await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo)
   }
 
   if (chat.bienvenida && m.messageStubType == 28) {
-    if (chat.sBye) {
-      let user = `@${m.messageStubParameters[0].split`@`[0]}`
-      let bye = chat.sBye.replace('@user', () => user);
-      await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
-    } else {
-      let bye = `╔══⏤͟͟͞͞𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕 \n║「 ADIOS 👋 」\n╚╦★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   ║✑  Se fue\n   ║✑ Jamás te quisimos aquí\n   *╚═══❖•⏤͟͟͞͞𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕•❖═══╝*`
-      await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
-    }
+    let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo)
   }
 
   if (chat.bienvenida && m.messageStubType == 32) {
-    if (chat.sBye) {
-      let user = `@${m.messageStubParameters[0].split`@`[0]}`
-      let bye = chat.sBye.replace('@user', () => user);
-      await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal)
-    } else {
-      let kick = `╔══⏤͟͟͞͞𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕 \n║〘 ADIOS 👋 〙\n╚╦✎ 〘 @${m.messageStubParameters[0].split`@`[0]} 〙\n   ║✎  Se fue\n   ║✇ Jamás te quisimos aquí\n   *╚═══❖•⏤͟͟͞͞𝑻𝒆𝒄𝒏𝒐-𝑩𝒐𝒕•❖═══╝*`
-      await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal)
-    }
-}} 
+    let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo)
+}}

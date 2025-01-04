a4ff5a256b9c8335425b29138376a74777ef49a9
Deylinel <deylibaquedano40@gmail.com>
2025-01-04 13:50:03 -0600

    Actualizar convertidor-tts.js

--- plugins/convertidor-tts.js
+++ plugins/convertidor-tts.js
@@ -1,12 +1,11 @@
 import textToSpeech from '@google-cloud/text-to-speech';
-import fs from 'fs';
-import util from 'util';
-import { join } from 'path';
+import {writeFileSync, unlinkSync} from 'fs';
+import {join} from 'path';
 
-const client = new textToSpeech.TextToSpeechClient();
 const defaultLang = 'es';
+const client = new textToSpeech.TextToSpeechClient();
 
-const handler = async (m, { conn, args, usedPrefix, command }) => {
+const handler = async (m, {conn, args, usedPrefix, command}) => {
   let lang = args[0];
   let text = args.slice(1).join(' ');
   if ((args[0] || '').length !== 2) {
@@ -14,19 +13,18 @@ const handler = async (m, { conn, args, usedPrefix, command }) => {
     text = args.join(' ');
   }
   if (!text && m.quoted?.text) text = m.quoted.text;
-
   let res;
   try {
-    res = await synthesizeSpeech(text, lang);
+    res = await tts(text, lang);
   } catch (e) {
-    m.reply(e.message);
+    m.reply(e + '');
+    text = args.join(' ');
     if (!text) throw `*🌟 Te Faltó Un Texto*\n\nEjemplo:\n- !tts Hola Tecno`;
-    res = await synthesizeSpeech(text, defaultLang);
+    res = await tts(text, defaultLang);
   } finally {
-    if (res) conn.sendFile(m.chat, res, 'tts.mp3', null, m, true);
+    if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true);
   }
 };
-
 handler.help = ['tts <lang> <teks>'];
 handler.tags = ['transformador'];
 handler.group = true;
@@ -34,17 +32,24 @@ handler.register = true;
 handler.command = ['tts'];
 export default handler;
 
-async function synthesizeSpeech(text, lang = 'es') {
-  const request = {
-    input: { text },
-    voice: { languageCode: lang, ssmlGender: 'NEUTRAL' },
-    audioConfig: { audioEncoding: 'MP3' },
-  };
-
-  const [response] = await client.synthesizeSpeech(request);
-  const filePath = join(global.__dirname(import.meta.url), '../tmp', `${Date.now()}.mp3`);
-  await util.promisify(fs.writeFile)(filePath, response.audioContent, 'binary');
-  const audioBuffer = fs.readFileSync(filePath);
-  fs.unlinkSync(filePath);
-  return audioBuffer;
+async function tts(text, lang = 'es') {
+  console.log(lang, text);
+  try {
+    const request = {
+      input: {text},
+      voice: {
+        languageCode: lang,
+        ssmlGender: 'MALE', // Cambia a 'FEMALE' para voz femenina.
+      },
+      audioConfig: {
+        audioEncoding: 'LINEAR16', // Cambia a otros formatos si es necesario.
+      },
+    };
+    const [response] = await client.synthesizeSpeech(request);
+    const filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date()) + '.wav');
+    writeFileSync(filePath, response.audioContent, 'binary');
+    return readFileSync(filePath);
+  } catch (e) {
+    throw e;
+  }
 }

2a64da0adaddf5c5a4a6b70356a919625bbbc2ba
Deylinel <deylibaquedano40@gmail.com>
2025-01-05 11:19:27 -0600

    Actualizar downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,46 +1,52 @@
 import { igdl } from 'ruhend-scraper'
 
-const handler = async (m, { text, conn, args, usedPrefix, command }) => {
-if (!args[0]) {
-return conn.reply(m.chat, '🌸 *Ingresa Un Link De Facebook*', m, rcanal)}
-let res
-try {
-conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-await m.react(rwait)
-res = await igdl(args[0])
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🥀 *Error al obtener datos. Verifica el enlace.*', m, fake)}
-let result = res.data
-if (!result || result.length === 0) {
-return conn.reply(m.chat, '🌹 *No se encontraron resultados.*', m, fake)}
-let data
-try {
-await m.react(rwait)
-data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)")
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🥀 *Error al procesar los datos.*', m, rcanal)}
-if (!data) {
-return conn.reply(m.chat, '🌹 *No se encontró una resolución adecuada.*', m, rcanal)}
-let video = data.url
-try {
-await m.react(rwait)
-await conn.sendMessage(m.chat, { video: { url: video }, caption: '🌷 *Tu video de facebook.*\n' + textbot, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: fkontak })
-await m.react(done)
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🥀 *Error al enviar el video.*', m, rcanal)}}
+const handler = async (m, { text, conn, args }) => {
+  if (!args[0]) {
+    return conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}🐈 𝗘𝗻𝘃𝗶́𝗮 𝗲𝗹 𝗹𝗶𝗻𝗸 𝗱𝗲𝗹 𝘃𝗶𝗱𝗲𝗼 𝗱𝗲 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸.`, m)
+  }
+
+  let res;
+  try {
+    await m.react('⏱️')
+    res = await igdl(args[0]);
+  } catch (e) {
+    return conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}⚠️ 𝗘𝗹 𝗲𝗻𝗹𝗮𝗰𝗲 𝗻𝗼 𝗲𝘀 𝘃𝗮́𝗹𝗶𝗱𝗼, 𝘃𝗲𝗿𝗶𝗳𝗶𝗾𝘂𝗲 𝘀𝗶 𝗲𝘀 𝘂𝗻 𝗲𝗻𝗹𝗮𝗰𝗲 𝗱𝗲 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸.`, m)
+    await m.react('❎️')
+  }
+
+  let result = res.data;
+  if (!result || result.length === 0) {
+    return conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}⚠️ 𝗡𝗼 𝘀𝗲 𝗲𝗻𝗰𝗼𝗻𝘁𝗿𝗮𝗿𝗼𝗻 𝗿𝗲𝘀𝘂𝗹𝘁𝗮𝗱𝗼𝘀 𝗱𝗲𝗹 𝘃𝗶𝗱𝗲𝗼.`, m)
+    await m.react('❎️')
+  }
+
+  let data;
+  try {
+    data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
+  } catch (e) {
+    return conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}⚠️ 𝗗𝗮𝘁𝗼𝘀 𝗻𝗼 𝗲𝗻𝗰𝗼𝗻𝘁𝗿𝗮𝗱𝗼𝘀.`, m)
+    await m.react('❎️')
+  }
+
+  if (!data) {
+    return conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}⚠️ 𝗡𝗼 𝘀𝗲 𝗲𝗻𝗰𝗼𝗻𝘁𝗿𝗼́ 𝗿𝗲𝘀𝘂𝗹𝘁𝗮𝗱𝗼𝘀 𝗱𝗲𝗹 𝘃𝗶𝗱𝗲𝗼.`, m)
+    await m.react('❎️')
+  }
+
+  let video = data.url;
+  try {
+    await conn.sendMessage(m.chat, { video: { url: video }, caption: `${lenguajeGB['smsAvisoEG']()}🏖 𝘼𝙦𝙪𝙞́ 𝙩𝙞𝙚𝙣𝙚𝙨 𝙩𝙪 𝙫𝙞𝙙𝙚𝙤 𝙙𝙚 𝙁𝙖𝙘𝙚𝙗𝙤𝙤𝙠.\n${wm}`, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
+    await m.react('✅️')
+  } catch (e) {
+    return conn.reply(m.chat, `${lenguajeGB['smsAvisoFG']()}❎️ 𝗢𝗰𝘂𝗿𝗿𝗶𝗼́ 𝘂𝗻 𝗲𝗿𝗿𝗼𝗿 𝗮𝗹 𝗱𝗲𝘀𝗰𝗮𝗿𝗴𝗮𝗿 𝗲𝗹 𝘃𝗶𝗱𝗲𝗼.`, m)
+    await m.react('❎️')
+  }
+}
 
 handler.help = ['facebook', 'fb']
 handler.tags = ['descargas']
 handler.command = ['facebook', 'fb']
 handler.register = true
-handler.group = true
+handler.limit = true
 
 export default handler

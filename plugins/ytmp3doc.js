bf6c48f6931152ed9490dab41b9a1d4c44911576
Deylinel <deylibaquedano40@gmail.com>
2025-01-04 20:08:13 -0600

    Actualizar ytmp3doc.js

--- plugins/ytmp3doc.js
+++ plugins/ytmp3doc.js
@@ -1,57 +1,19 @@
-import yts from 'yt-search';
-import fetch from "node-fetch";
+import fetch from 'node-fetch'
 
-const handler = async (m, { text, usedPrefix, command, conn }) => {
-    if (!text) {
-        throw await m.reply("✨ Ingresa una consulta o link de *YouTube*");
-    }
-    await m.react('🕓');
+let HS = async (m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 
-    let res = await yts(text);
-    let videoList = res.all;
-    let videos = videoList[0];
+try {
+let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp3?url=${text}`)
+let json = await api.json()
+let title = json.result.metadata.title
+let dl_url = json.result.download.url
+await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
 
-    async function ytdl(url) {
-        const response = await fetch('https://restapi.apibotwa.biz.id/api/ytmp3?url=$', {
-            method: 'POST',
-            headers: {
-                'accept': '*/*',
-                'api_key': 'free',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                text: url
-            })
-        });
+} catch (error) {
+console.error(error)
+}}
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+HS.command = ['ytmp3']
 
-        const data = await response.json();
-        return data;
-    }
-
-    let data_play = await ytdl(videos.url);
-    console.log(data_play);
-
-    if (data_play && data_play.data && data_play.data.mp3) {
-        await conn.sendMessage(m.chat, { 
-            document: { url: data_play.data.mp3 }, 
-            mimetype: 'audio/mp3', 
-            fileName: `${videos.title}.mp3`
-        }, { quoted: m });
-
-        await m.react('✅'); 
-    } else {
-        //await m.reply("❌ No se pudo obtener el audio.");
-        await m.react('❌'); 
-    }
-};
-
-handler.help = ['ytmp3doc <yt url>'];
-handler.tags = ['downloader'];
-handler.command = ['ytmp3doc'];
-handler.register = true;
-
-export default handler;
+export default HS

7d7f29a21f8f8ba3f016a672f20adc3d67181f28
Deylinel <deylibaquedano40@gmail.com>
2025-01-06 13:58:09 -0600

    Actualizar download-ig.js

--- plugins/download-ig.js
+++ plugins/download-ig.js
@@ -1,49 +1,30 @@
-import { igdl } from "ruhend-scraper";
+import { igdl } from "ruhend-scraper"
 
 let handler = async (m, { args, conn }) => { 
-    if (!args[0]) {
-        return conn.reply(m.chat, '🔗 *Por favor, proporciona un enlace de Instagram válido.*', m, rcanal);
-    }
-    try {
-        await m.react(rwait);
-        conn.reply(m.chat, `🕒 *Procesando tu video, por favor espera...*`, m, {
-            contextInfo: { 
-                externalAdReply: { 
-                    mediaUrl: null, 
-                    mediaType: 1, 
-                    showAdAttribution: true,
-                    title: packname,
-                    body: wm,
-                    previewType: 0, 
-                    thumbnail: icons,
-                    sourceUrl: channel 
-                }
-            }
-        });      
+if (!args[0]) {
+return conn.reply(m.chat, '☁️ *Ingresa un link de Instagram*', m, rcanal)}
+try {
+await m.react(rwait)
+conn.reply(m.chat, `⚙️ *Enviando El Video...*`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})      
+let res = await igdl(args[0])
+let data = res.data       
+for (let media of data) {
+await new Promise(resolve => setTimeout(resolve, 2000))           
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🚀 *Tu video de instagram.*\n' + textbot, fkontak)
+}} catch {
+await m.react(error)
+conn.reply(m.chat, '⚙️ Ocurrió un error.', m, fake)}}
 
-        let res = await igdl(args[0]);
-        let data = res.data;       
-        for (let media of data) {
-            await new Promise(resolve => setTimeout(resolve, 2000));           
-            await conn.sendFile(
-                m.chat, 
-                media.url, 
-                'instagram.mp4', 
-                '🎥 *Aquí está tu video descargado de Instagram.*\n' + textbot, 
-                fkontak
-            );
-        }
-    } catch {
-        await m.react(error);
-        conn.reply(m.chat, '⚙️ *Ocurrió un error al procesar el enlace, intenta nuevamente.*', m, fake);
-    }
-};
-
-handler.command = ['instagram2', 'ig2'];
-handler.tags = ['descargas'];
-handler.help = ['instagram2', 'ig2'];
+handler.command = ['instagram2', 'ig2']
+handler.tags = ['descargas']
+handler.help = ['instagram2', 'ig2']
 //handler.yenes = 1
 handler.group = true;
-handler.register = true;
+handler.register = true
 
-export default handler;
+export default handler

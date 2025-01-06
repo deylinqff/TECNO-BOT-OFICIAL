8d15136936fa1ea546fda9a6ab059a8b62a3ffa3
Deylinel <deylibaquedano40@gmail.com>
2025-01-06 09:51:16 -0600

    Actualizar _Accept-link.js

--- plugins/_Accept-link.js
+++ plugins/_Accept-link.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text }) => {
   m.react('⚙️');
 
   // Número autorizado (sin espacios ni caracteres especiales)
-  const allowedNumber = ${user};
+  const allowedNumber = '50488198573';
 
   // Verifica si el mensaje proviene de tu número
   if (m.sender.split('@')[0] !== allowedNumber) {

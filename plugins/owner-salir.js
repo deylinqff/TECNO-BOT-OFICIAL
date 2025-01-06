7fa0bbbf2bf6cf919b0af5280166939a7a77ca18
Deylinel <deylibaquedano40@gmail.com>
2025-01-05 22:45:25 -0600

    Actualizar owner-salir.js

--- plugins/owner-salir.js
+++ plugins/owner-salir.js
@@ -1,12 +1,16 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
-await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*ADIOS, CROWBOT SE DESPIDE!💫👋*', mentions: [m.sender] }, { quoted: estilo })
-await conn.groupLeave(id)}
-handler.help = ['salir']
-handler.tags = ['owner']
-handler.command = /^(salir|out|leavegc|leave|salirdelgrupo)$/i
+let chat = global.db.data.chats[m.chat]
+chat.welcome = false
+await conn.reply(id, `🚀 *ADIOS A TODOS, TECNO-BOT SE DESPIDE! (≧ω≦)ゞ*`) 
+await conn.groupLeave(id)
+try {  
+chat.welcome = true
+} catch (e) {
+await m.reply(`${fg}`) 
+return console.log(e)
+}}
+handler.command = ['salir','leavegc','salirdelgrupo','leave']
 handler.group = true
 handler.rowner = true
-
 export default handler

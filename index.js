c96e417b815983c56b30957fafcfab19344450ff
Deylinel <deylibaquedano40@gmail.com>
2025-01-09 15:21:20 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -7,29 +7,27 @@ import cfonts from 'cfonts';
 import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
-console.log('\n✰ Iniciando TECNO-BOT ✰');
+console.log('\n✰ Iniciando TECNO ✰');
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-
-say('TECNO\nST', {
+say('TECNO\nBOT', {
 font: 'block',
 align: 'center',
-gradient: ['yellow', 'magenta']
-})
-say(`StarCore-Team`, {
+colors: ['white']
+});
+say(`Multi Device`, {
 font: 'chrome',
 align: 'center',
-colors: ['red', 'magenta']
+colors: ['red']
 });
-say(`Powered By Deylin`, {
+say(`Developed By • Deylin`, {
 font: 'console',
 align: 'center',
 colors: ['magenta']
 });
-
 var isRunning = false;
 function start(file) {
 if (isRunning) return;
@@ -59,7 +57,7 @@ break;
 });
 p.on('exit', (_, code) => {
 isRunning = false;
-console.error('🎈 Error:\n', code);
+console.error('🚩 Error:\n', code);
 process.exit();
 if (code === 0) return;
 watchFile(args[0], () => {
@@ -75,7 +73,7 @@ p.emit('message', line.trim());
 }
 process.on('warning', (warning) => {
 if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('💎 Se excedió el límite de Listeners en:');
+console.warn('🚩 Se excedió el límite de Listeners en:');
 console.warn(warning.stack);
 }
 });

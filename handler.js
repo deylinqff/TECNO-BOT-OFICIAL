f8ce7acf4c54d800a06078ce84a99a0f02e5fb64
Deylinel <deylibaquedano40@gmail.com>
2025-01-10 09:59:58 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -415,7 +415,8 @@ global.dfail = (type, m, conn, usedPrefix) => {
         private: "> _*`🚀Ve a mi chat privado y usa este comando`*_",
         admin: "> _*`🚀Quien eres?, tu no eres admin`*_",
         botAdmin: "> _*`🚀Es necesario que sea admin primero para usar esta función`*_",
-        unreg: "> _*`🚀USUARIO NO REGISTRADO⚡`*_\n\n`Para registrarse:`\n\n> .reg nombre.edad\n\n`Ejemplo:`\n\n> .reg Deyli. 15",
+        unreg: "> _*`🚀USUARIO NO REGISTRADO⚡`*_\n\n`Para registrarse:`\n\n> .reg nombre.edad\n\n`Ejemplo:`\n\n> .reg Deyli. 15
+únete a mi canal https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m",
         restrict: "> _*`⚡Comando desactivado por mi Owner`*_" 
     }[type]
     if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))

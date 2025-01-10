4095a7b86df321da05ae137abbe799c926b475d9
Deylinel <deylibaquedano40@gmail.com>
2025-01-10 11:21:28 -0600

    Actualizar ytmp4.js

--- plugins/ytmp4.js
+++ plugins/ytmp4.js
@@ -1,19 +1,27 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
 let HS = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `⚡ Ingresa un link de youtube`, m)
+  if (!text) return conn.reply(m.chat, `❀ Ingresa un link de youtube`, m)
 
-try {
-let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp4?url=${text}`)
-let json = await api.json()
-let title = json.data.metadata.title
-let dl_url = json.data.download.url
-await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${json.data.filename}.mp4`, mimetype: "video/mp4" }, { quoted: m })
+  try {
+    let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp3?url=${text}`)
+    let json = await api.json()
+    let title = json.result.metadata.title
+    let dl_url = json.result.download.url
+    await conn.sendMessage(m.chat, { audio: { url: dl_url }, fileName: `${title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m })
 
-} catch (error) {
-console.error(error)
-}}
+    // Reacción con 🚀
+    await conn.sendMessage(m.chat, { react: { text: '🚀', key: m.key } })
 
-HS.command = ['ytmp4']
+  } catch (error) {
+    console.error(error)
+
+    // Enviar un sticker en caso de error
+    const errorSticker = 'https://path_to_your_sticker.png' // Cambia por la URL de tu sticker
+    await conn.sendMessage(m.chat, { sticker: { url: errorSticker } }, { quoted: m })
+  }
+}
+
+HS.command = ['ytmp3']
 
 export default HS

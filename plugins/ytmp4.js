4c91a62cd157f38c78b146714234d321b4a16523
Deylinel <deylibaquedano40@gmail.com>
2025-01-10 11:18:29 -0600

    Actualizar ytmp4.js

--- plugins/ytmp4.js
+++ plugins/ytmp4.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 let HS = async (m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, `🚀 Ingresa un link de youtube`, m)
+if (!text) return conn.reply(m.chat, `⚡ Ingresa un link de youtube`, m)
 
 try {
 let api = await fetch(`https://restapi.apibotwa.biz.id/api/ytmp4?url=${text}`)

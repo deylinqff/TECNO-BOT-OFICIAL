d431dd10fffa7bfa75935b1c3dc7ab1397cbd1ff
Deylinel <deylibaquedano40@gmail.com>
2025-01-15 14:31:10 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,52 +1,34 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), estilo, { contextInfo: { externalAdReply: { showAdAttribution: true }}})
-
-
-}
-
-handler.help = ['creador']
-handler.tags = ['info']
-handler.command = ['creador', 'creator', 'owner', 'propietario', 'dueño']
-
-
-export default handler
-
-/*import PhoneNumber from 'awesome-phonenumber'
+import PhoneNumber from 'awesome-phonenumber'
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+m.react('☁️')
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-let biografia = await conn.fetchStatus('50488198573' +'@s.whatsapp.net').catch(_ => '𝑯𝒐𝒍𝒂 𝒎𝒆 𝒍𝒍𝒂𝒎𝒐 𝑫𝒆𝒚𝒍𝒊𝒏 𝒚 𝒔𝒐𝒚 𝒅𝒖𝒆𝒏̃𝒐 𝒅𝒆 𝐓𝐄𝐂𝐍𝐎-𝐁𝐎𝐓')
-let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}` +'@s.whatsapp.net').catch(_ => '𝑯𝒐𝒍𝒂 𝒎𝒆 𝒍𝒍𝒂𝒎𝒐 𝑫𝒆𝒚𝒍𝒊𝒏 𝒚 𝒔𝒐𝒚 𝒅𝒖𝒆𝒏̃𝒐 𝒅𝒆 𝐓𝐄𝐂𝐍𝐎-𝐁𝐎𝐓')
-let bio = biografia.status?.toString() || 'Hσ𝑯𝒐𝒍𝒂 𝒎𝒆 𝒍𝒍𝒂𝒎𝒐 𝑫𝒆𝒚𝒍𝒊𝒏 𝒚 𝒔𝒐𝒚 𝒅𝒖𝒆𝒏̃𝒐 𝒅𝒆 𝐓𝐄𝐂𝐍𝐎-𝐁𝐎𝐓'
-let biobot = biografiaBot.status?.toString() || '𝑯𝒐𝒍𝒂 𝒎𝒆 𝒍𝒍𝒂𝒎𝒐 𝑫𝒆𝒚𝒍𝒊𝒏 𝒚 𝒔𝒐𝒚 𝒅𝒖𝒆𝒏̃𝒐 𝒅𝒆 𝐓𝐄𝐂𝐍𝐎-𝐁𝐎𝐓'
+let pp = await conn.profilePictureUrl(who).catch(_ => 'https://files.catbox.moe/3kbbok.jpg')
+let biografia = await conn.fetchStatus('584120346669' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}` +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
+let bio = biografia.status?.toString() || 'Sin Biografía'
+let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 let name = await conn.getName(who)
 
-await sendContactArray(conn, m.chat, [
-[`50488198573`, `⚙️ Propietario`, `🚀 𝐃𝐄𝐘𝐋𝐈𝐍 `, dev, correo, `🎌 Tokio`, `${yt}`, bio],
-[`${conn.user.jid.split('@')[0]}`, `𝑬𝑺 𝑼𝑵 𝑩𝑶𝑻`, `${wm}`, `📵 𝑵𝑶 𝑯𝑨𝑺𝑬𝑹 𝑺𝑷𝑨𝑴`, correo, `🎌 Tokio`, md, biobot]
+  await sendContactArray(conn, m.chat, [
+     [`${nomorown}`, `👑 Propietario`, `☁️ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜`, dev, 'thekingdestroy507@gmail.com', `🇻🇪 Venezuela`, `https://github.com/The-King-Destroy`, bio],
+[`${conn.user.jid.split('@')[0]}`, `Es Un Bot 🍬`, `${packname}`, `📵 No Hacer Spam`, 'moisesmusic04@gmail.com', `🇨🇴 Colombia`, `https://github.com/The-King-Destroy/Yuki_Suou-Bot`, biobot]
 ], m)
+  } 
 
-await conn.reply(m.chat,  `*❕ ${name} él es mi dueño, si quieres reportar un error contáctalo*`, m, fake)
-}
-
-handler.help = ['owner']
-handler.tags = ['info']
-handler.command = ['owner', 'creator']
-// handler.estrellas = 1
-
+handler.help = ["creador","owner"]
+handler.tags = ["info"]
+handler.command = ['creador', 'owner']
 export default handler
 
 async function sendContactArray(conn, jid, data, quoted, options) {
-if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
-let contacts = []
-for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
-number = number.replace(/[^0-9]/g, '')
-let njid = number + '@s.whatsapp.net'
-let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
-// N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
-let vcard = `
+        if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
+                let contacts = []
+        for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
+            number = number.replace(/[^0-9]/g, '')
+            let njid = number + '@s.whatsapp.net'
+            let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+            let vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
@@ -58,17 +40,21 @@ item2.EMAIL;type=INTERNET:${isi2}
 item2.X-ABLabel:📧 Email
 item3.ADR:;;${isi3};;;;
 item3.X-ABADR:ac
-item3.X-ABLabel:⭐️ Region
+item3.X-ABLabel:🏷 Region
 item4.URL:${isi4}
 item4.X-ABLabel:Website
 item5.X-ABLabel:${isi5}
 END:VCARD`.trim()
-contacts.push({ vcard, displayName: name })}
-return await conn.sendMessage(jid, {
-contacts: {
-displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
-contacts,
-}},
-{ quoted, 
-...options
-})}*/
+            contacts.push({ vcard, displayName: name })
+        }
+        return await conn.sendMessage(jid, {
+            contacts: {
+                displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
+                contacts,
+            }
+        },
+        {
+            quoted,
+            ...options
+        })
+}
